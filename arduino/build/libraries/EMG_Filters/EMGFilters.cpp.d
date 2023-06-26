06062692305932ff245af78907b634930ed8a8de
scinvaro <rulesky_kim@snu.ac.kr>
2023-06-26 16:35:40 +0900

    Chore : Add .gitignore for arduino/build/

--- arduino/build/libraries/EMG_Filters/EMGFilters.cpp.d
+++
@@ -1,3 +0,0 @@
-c:\Users\rulet\Documents\projects\AR\ spatial\ computing\ Interface\arduino\build\libraries\EMG_Filters\EMGFilters.cpp.o: \
- C:\Users\rulet\Documents\Arduino\libraries\EMG_Filters\EMGFilters.cpp \
- C:\Users\rulet\Documents\Arduino\libraries\EMG_Filters\EMGFilters.h

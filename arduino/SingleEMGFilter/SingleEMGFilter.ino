e64f98ed2f5aa5357e299359666d02b51689f4bf
scinvaro <rulesky_kim@snu.ac.kr>
2023-06-26 14:19:49 +0900

    first commit

---
+++ arduino/SingleEMGFilter/SingleEMGFilter.ino
@@ -0,0 +1,112 @@
+/*
+* Copyright 2017, OYMotion Inc.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in
+*    the documentation and/or other materials provided with the
+*    distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+* THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+* DAMAGE.
+*
+*/
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include "EMGFilters.h"
+
+#define TIMING_DEBUG 1
+
+#define SensorInputPin1 A1 // input pin number
+
+EMGFilters myFilter1;
+// discrete filters must works with fixed sample frequence
+// our emg filter only support "SAMPLE_FREQ_500HZ" or "SAMPLE_FREQ_1000HZ"
+// other sampleRate inputs will bypass all the EMG_FILTER
+SAMPLE_FREQUENCY sampleRate = SAMPLE_FREQ_1000HZ;
+// For countries where power transmission is at 50 Hz
+// For countries where power transmission is at 60 Hz, need to change to
+// "NOTCH_FREQ_60HZ"
+// our emg filter only support 50Hz and 60Hz input
+// other inputs will bypass all the EMG_FILTER
+NOTCH_FREQUENCY humFreq = NOTCH_FREQ_50HZ;
+
+// Calibration:
+// put on the sensors, and release your muscles;
+// wait a few seconds, and select the max value as the Threshold;
+// any value under Threshold will be set to zero
+static int Threshold1 = 0;
+
+unsigned long timeStamp;
+unsigned long timeBudget;
+
+void setup() {
+    /* add setup code here */
+    myFilter1.init(sampleRate, humFreq, true, true, true);
+
+    // open serial
+    Serial.begin(115200);
+
+    // setup for time cost measure
+    // using micros()
+    timeBudget = 1e6 / sampleRate;
+    // micros will overflow and auto return to zero every 70 minutes
+}
+
+void loop() {
+    /* add main program code here */
+    // In order to make sure the ADC sample frequence on arduino,
+    // the time cost should be measured each loop
+    /*------------start here-------------------*/
+    timeStamp = micros();
+
+    int Value1 = analogRead(SensorInputPin1);
+
+    // filter processing
+    int DataAfterFilter1 = myFilter1.update(Value1);
+
+    int envlope1 = sq(DataAfterFilter1);
+    // any value under Threshold will be set to zero
+    envlope1 = (envlope1 > Threshold1) ? envlope1 : 0;
+
+    timeStamp = micros() - timeStamp;
+    if (TIMING_DEBUG) {
+        // Serial.print("Read Data: "); Serial.println(Value);
+        // Serial.print("Filtered Data: ");Serial.println(DataAfterFilter);
+        // Serial.print("Squared Data: ");
+        Serial.print(envlope1);
+        Serial.print("\t");
+        Serial.println(2500); // y-axis scale 고정을 위한 constant
+        // Serial.print("Filters cost time: "); Serial.println(timeStamp);
+        // the filter cost average around 520 us
+    }
+
+    /*------------end here---------------------*/
+    // if less than timeBudget, then you still have (timeBudget - timeStamp) to
+    // do your work
+    delayMicroseconds(500);
+    // if more than timeBudget, the sample rate need to reduce to
+    // SAMPLE_FREQ_500HZ
+}

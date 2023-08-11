c368ae3a380756d14133cef567e19f74049421eb
scinvaro <rulesky_kim@snu.ac.kr>
2023-08-11 15:34:34 +0900

    Feat : Transfer IMU data from peripheral to central using BLE

--- arduino/Seeeduino_nRF52840/Seeeduino_nRF52840/Seeeduino_nRF52840.ino
+++ arduino/Seeeduino_nRF52840/Seeeduino_nRF52840/Seeeduino_nRF52840.ino
@@ -27,18 +27,79 @@
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 /*******************************************************************************/
+//===============================BLUETOOTH BEGINS===============================//
+#include <Adafruit_LittleFS.h>
+#include <InternalFileSystem.h>
+#include <bluefruit.h>
+
+// BLE Service
+BLEDfu bledfu;   // OTA DFU service
+BLEDis bledis;   // device information
+BLEUart bleuart; // uart over ble
+BLEBas blebas;   // battery
 
 #include "LSM6DS3.h"
 #include "Wire.h"
+//===============================BLUETOOTH ENDS===============================//
 
-//Create a instance of class LSM6DS3
-LSM6DS3 myIMU(I2C_MODE, 0x6A);    //I2C device address 0x6A
+// Create a instance of class LSM6DS3
+LSM6DS3 myIMU(I2C_MODE, 0x6A); // I2C device address 0x6A
 
 void setup() {
+    //===============================BLUETOOTH BEGINS===============================//
+    Serial.begin(115200);
+
+#if CFG_DEBUG
+    // Blocking wait for connection when debug mode is enabled via IDE
+    while (!Serial)
+        yield();
+#endif
+
+    Serial.println("Bluefruit52 BLEUART Example");
+    Serial.println("---------------------------\n");
+
+    // Setup the BLE LED to be enabled on CONNECT
+    // Note: This is actually the default behavior, but provided
+    // here in case you want to control this LED manually via PIN 19
+    Bluefruit.autoConnLed(true);
+
+    // Config the peripheral connection with maximum bandwidth
+    // more SRAM required by SoftDevice
+    // Note: All config***() function must be called before begin()
+    Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
+
+    Bluefruit.begin();
+    Bluefruit.setTxPower(4); // Check bluefruit.h for supported values
+    // Bluefruit.setName(getMcuUniqueID()); // useful testing with multiple central connections
+    Bluefruit.Periph.setConnectCallback(connect_callback);
+    Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
+
+    // To be consistent OTA DFU should be added first if it exists
+    bledfu.begin();
+
+    // Configure and Start Device Information Service
+    bledis.setManufacturer("Adafruit Industries");
+    bledis.setModel("Bluefruit Feather52");
+    bledis.begin();
+
+    // Configure and Start BLE Uart Service
+    bleuart.begin();
+
+    // Start BLE Battery Service
+    blebas.begin();
+    blebas.write(100);
+
+    // Set up and start advertising
+    startAdv();
+
+    Serial.println("Please use Adafruit's Bluefruit LE app to connect in UART mode");
+    Serial.println("Once connected, enter character(s) that you wish to send");
+    //===============================BLUETOOTH ENDS===============================//
+
     // put your setup code here, to run once:
-    Serial.begin(9600);
-    while (!Serial);
-    //Call .begin() to configure the IMUs
+    // while (!Serial)
+    //     ;
+    // Call .begin() to configure the IMUs
     if (myIMU.begin() != 0) {
         Serial.println("Device error");
     } else {
@@ -47,7 +108,7 @@ void setup() {
 }
 
 void loop() {
-    //Accelerometer
+    // Accelerometer
     Serial.print("\nAccelerometer:\n");
     Serial.print(" X1 = ");
     Serial.println(myIMU.readFloatAccelX(), 4);
@@ -56,7 +117,7 @@ void loop() {
     Serial.print(" Z1 = ");
     Serial.println(myIMU.readFloatAccelZ(), 4);
 
-    //Gyroscope
+    // Gyroscope
     Serial.print("\nGyroscope:\n");
     Serial.print(" X1 = ");
     Serial.println(myIMU.readFloatGyroX(), 4);
@@ -65,12 +126,71 @@ void loop() {
     Serial.print(" Z1 = ");
     Serial.println(myIMU.readFloatGyroZ(), 4);
 
-    //Thermometer
-    Serial.print("\nThermometer:\n");
-    Serial.print(" Degrees C1 = ");
-    Serial.println(myIMU.readTempC(), 4);
-    Serial.print(" Degrees F1 = ");
-    Serial.println(myIMU.readTempF(), 4);
+    // Thermometer
+    // Serial.print("\nThermometer:\n");
+    // Serial.print(" Degrees C1 = ");
+    // Serial.println(myIMU.readTempC(), 4);
+    // Serial.print(" Degrees F1 = ");
+    // Serial.println(myIMU.readTempF(), 4);
 
-    delay(40);
+    String resultant;
+    resultant = String(myIMU.readFloatAccelX()) + "/" + myIMU.readFloatAccelY() + "/" + myIMU.readFloatAccelZ() + +myIMU.readFloatGyroX() + "/" +
+                myIMU.readFloatGyroY() + "/" + myIMU.readFloatGyroZ();
+    bleuart.println(resultant);
+    // delay(20);
 }
+
+//===============================BLUETOOTH BEGINS===============================//
+void startAdv(void) {
+    // Advertising packet
+    Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+    Bluefruit.Advertising.addTxPower();
+
+    // Include bleuart 128-bit uuid
+    Bluefruit.Advertising.addService(bleuart);
+
+    // Secondary Scan Response packet (optional)
+    // Since there is no room for 'Name' in Advertising packet
+    Bluefruit.ScanResponse.addName();
+
+    /* Start Advertising
+     * - Enable auto advertising if disconnected
+     * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+     * - Timeout for fast mode is 30 seconds
+     * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+     *
+     * For recommended advertising interval
+     * https://developer.apple.com/library/content/qa/qa1931/_index.html
+     */
+    Bluefruit.Advertising.restartOnDisconnect(true);
+    Bluefruit.Advertising.setInterval(32, 244); // in unit of 0.625 ms
+    Bluefruit.Advertising.setFastTimeout(30);   // number of seconds in fast mode
+    Bluefruit.Advertising.start(0);             // 0 = Don't stop advertising after n seconds
+}
+
+// callback invoked when central connects
+void connect_callback(uint16_t conn_handle) {
+    // Get the reference to current connection
+    BLEConnection *connection = Bluefruit.Connection(conn_handle);
+
+    char central_name[32] = {0};
+    connection->getPeerName(central_name, sizeof(central_name));
+
+    Serial.print("Connected to ");
+    Serial.println(central_name);
+}
+
+/**
+ * Callback invoked when a connection is dropped
+ * @param conn_handle connection where this event happens
+ * @param reason is a BLE_HCI_STATUS_CODE which can be found in ble_hci.h
+ */
+void disconnect_callback(uint16_t conn_handle, uint8_t reason) {
+    (void)conn_handle;
+    (void)reason;
+
+    Serial.println();
+    Serial.print("Disconnected, reason = 0x");
+    Serial.println(reason, HEX);
+}
+//===============================BLUETOOTH ENDS===============================//
